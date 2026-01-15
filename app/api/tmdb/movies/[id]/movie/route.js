82aaf530cb568d5a447893beb1f0fbe9102d30de
danny <dnioko0412@gmail.com>
2026-01-15 13:20:09 +0800

    Used components

---
+++ app/api/tmdb/movies/[id]/movie/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(req, { params }) {
+  try {
+    const { id } = params;
+    const token = process.env.TMDB_TOKEN;
+    const baseUrl = process.env.TMDB_BASE_URL;
+    if (!token || !baseUrl) {
+      return NextResponse.json(
+        { error: "Missing environment Variables" },
+        { status: 500 }
+      );
+    }
+
+    const url = `${baseUrl}/movie/${id}/credits?language=en-US`;
+    const res = await axios.get(url, {
+      headers: {
+        accept: "application/json",
+        Authorization: "application/json",
+      },
+    });
+    return NextResponse.json(res, { status: 200 });
+  } catch (err) {
+    const status = err?.response?.status || 500;
+    const message =
+      err?.response?.status.message || err?.message || "Internal Server Error";
+    return NextResponse({ status }, { message });
+  }
+}
