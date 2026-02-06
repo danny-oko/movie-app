b467463ef378da9617b973128fedebf3da247767
dnii <123633815+danny-oko@users.noreply.github.com>
2026-02-06 10:49:39 +0800

    do not use this

--- app/similar/[id]/page.jsx
+++ app/similar/[id]/page.jsx
@@ -14,8 +14,8 @@ import { moviesService } from "@/lib/services/movies";
 
 export default function Page() {
   const params = useParams();
-  const movieId = Array.isArray(params.id) ? params.id[0] : params?.id;
-  console.log(movieId);
+  const id = Array.isArray(params.id) ? params.id[0] : params?.id;
+  // console.log(id);
 
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -24,30 +24,33 @@ export default function Page() {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-useEffect(() => {
-  if (!movieId) return;
+  // useEffect(() => {
+  //   if (!movieId) return;
 
-  (async () => {
-    try {
-      setLoading(true);
-      setError(null);
+  // (async () => {
+  //   try {
+  //     setLoading(true);
+  //     setError(null);
 
-      // const data = await moviesService.similar(movieId, page);
-      // setMovies(data?.results ?? []);
-      // setTotalPages(data?.total_pages ?? 1);
+  //     // const data = await moviesService.similar(movieId, page);
+  //     // setMovies(data?.results ?? []);
+  //     // setTotalPages(data?.total_pages ?? 1);
 
-      moviesService.similar(movieId,page).then((data) => {
-        console.log(data)
-      })
-    } catch (e) {
-      if (e.code === "ERR_CANCELED" || e.name === "CanceledError") return;
-      setError(e?.message || "Failed to load");
-    } finally {
-      setLoading(false);
-    }
-  })();
-}, [movieId, page]);
+  //       moviesService.similar(page).then((data) => {
+  //         setMovies(data?.results ?? []);
+  //         setTotalPages(data?.total_pages ?? 1);
+  //       });
+  //     } catch (e) {
+  //       if (e.code === "ERR_CANCELED" || e.name === "CanceledError") return;
+  //       setError(e?.message || "Failed to load");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
 
+  //   run();
+  //   return () => controller.abort();
+  // }, [id]);
 
   return (
     <div className="min-h-screen bg-background flex flex-col">
